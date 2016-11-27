087aa3be3278182a3e6074e43db91d7095f7d518
Elijah Lofgren <elijahlofgren@gmail.com>
2016-11-27 17:53:24 -0600

    Start using React.AspNet
    Following https://reactjs.net/getting-started/tutorial.html

--- TestAndRemember/Startup.cs
+++ TestAndRemember/Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +41,8 @@ namespace TestAndRemember
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
             services.AddMvc();
 
             var connectionString = Configuration["DbContextSettings:ConnectionString"];
@@ -71,7 +75,25 @@ namespace TestAndRemember
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //    .AddScript("~/Scripts/First.jsx")
+                //    .AddScript("~/Scripts/Second.jsx");
 
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //    .SetLoadBabel(false)
+                //    .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
             app.UseStaticFiles();
 
             app.UseIdentity();
