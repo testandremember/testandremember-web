4bedfad1f5bfe890af3496c41d093ad1a0c7e1f4
Elijah Lofgren <elijahlofgren@gmail.com>
2016-11-28 17:45:10 -0600

    Install react locally and start work on question add form using React.

---
+++ TestAndRemember/wwwroot/js/components/questions/add_question_form.jsx
@@ -0,0 +1,13 @@
+﻿var AddQuestionForm = React.createClass({
+    render: function() {
+        return (
+          <div className="add-question-form">
+            Hello, world! I am a question add form.
+          </div>
+      );
+    }
+});
+ReactDOM.render(
+  <AddQuestionForm />,
+  document.getElementById('add-question-form')
+);
